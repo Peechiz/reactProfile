d9e1d4b256812a63cdbb689120b37ba10f0d1389
Chris Impicciche <cimpicci@gmail.com>
2016-09-30 14:39:03 -0500

    adds correctly failing GET api/users test

--- test/profile.test.js
+++ test/profile.test.js
@@ -8,3 +8,15 @@ describe('tests are ready', ()=>{
     expect(1).to.equal(1)
   })
 })
+
+describe('GET api/users', ()=>{
+  it('should get all users', (done)=>{
+    request.get('/api/users')
+    .expect(200)
+    .end((err,res) => {
+      console.log(res.body);
+      expect(res.body).to.deep.equal(5)
+      done();
+    })
+  })
+})
