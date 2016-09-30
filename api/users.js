89b43e569b97b84de32befb64006327903c9fc3c
Chris Impicciche <cimpicci@gmail.com>
2016-09-30 14:34:38 -0500

    adds GET /users

--- api/users.js
+++ api/users.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const m = require('../models/index') // models
+const db = m.db;
+
+router.get('/users', (req,res)=>{
+  m.User.findAll().then(users => {
+    res.send(users.map(x => x.toJSON()))
+  })
+})
+
 module.exports = router;
