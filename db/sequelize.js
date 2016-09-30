abe49df1ba2c2b97f50afebbe46a7310d69d5a4c
Chris Impicciche <cimpicci@gmail.com>
2016-09-30 16:51:48 -0500

    overhauls the config to allow switching to testing DB

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,4 +1,22 @@
 const Sequelize = require('sequelize');
-const db = process.env.DATABASE_URL || 'postgres://localhost/sandbox';
-const sequelize = new Sequelize(db);
+
+if (process.env.DATABASE_URL) {
+  const sequelize = new Sequelize(process.env.DATABASE_URL);
+  module.exports = sequelize
+}
+
+let config;
+let db;
+
+if (process.env.DB_CONFIG){  // if testing
+  config = process.env.DB_CONFIG; // 'test'
+  db = 'postgres://localhost/sandbox_test'
+} else {
+  config = 'development';
+  db = 'postgres://localhost/sandbox'
+}
+
+const options = require('../config/config.json')[config]
+const sequelize = new Sequelize(db, {logging: false});
+
 module.exports = sequelize;
