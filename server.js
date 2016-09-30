decf6b1a60fd51160b073817df63f4a3ce88bd42
Chris Impicciche <cimpicci@gmail.com>
2016-09-30 16:53:27 -0500

    adds body parser

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
