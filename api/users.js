daf9a47bf3b8e48841a9a24432db999171f11f62
Chris Impicciche <cimpicci@gmail.com>
2016-09-30 16:50:20 -0500

    adds get and put to users/:id

--- api/users.js
+++ api/users.js
@@ -7,6 +7,29 @@ const db = m.db;
 router.get('/users', (req,res)=>{
   m.User.findAll().then(users => {
     res.send(users.map(x => x.toJSON()))
+  }).catch( err => {
+    res.send(err)
+  })
+})
+
+router.get('/users/:id', (req,res) => {
+  m.User.findAll({
+    where: {id: req.params.id}
+  }).then(users => {
+    res.send(users.map(x => x.toJSON())[0])
+  }).catch( err => {
+    res.send(err)
+  })
+})
+
+router.put('/users/:id', (req,res) => {
+  m.User.upsert({
+    id: req.params.id,
+    name: req.body.name,
+    img: req.body.img,
+    info: req.body.info
+  }).then( result => {
+    res.sendStatus(200);
   })
 })
 
