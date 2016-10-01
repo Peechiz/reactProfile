4365037f5cd699a0e3b9e5eeef486bbe60e547a8
Chris Impicciche <cimpicci@gmail.com>
2016-10-01 16:52:48 -0500

    adds else block

--- db/sequelize.js
+++ db/sequelize.js
@@ -4,20 +4,20 @@ const Sequelize = require('sequelize');
 if (process.env.DATABASE_URL) {
   const sequelize = new Sequelize(process.env.DATABASE_URL);
   module.exports = sequelize
-}
-
-let config;
-let db;
-
-if (process.env.DB_CONFIG){  // if testing
-  config = process.env.DB_CONFIG; // 'test'
-  db = 'postgres://localhost/sandbox_test'
 } else {
-  config = 'development';
-  db = 'postgres://localhost/sandbox'
+  let config;
+  let db;
+
+  if (process.env.DB_CONFIG){  // if testing
+    config = process.env.DB_CONFIG; // 'test'
+    db = 'postgres://localhost/sandbox_test'
+  } else {
+    config = 'development';
+    db = 'postgres://localhost/sandbox'
+  }
+
+  const options = require('../config/config.json')[config]
+  const sequelize = new Sequelize(db, {logging: false});
+
+  module.exports = sequelize;
 }
-
-const options = require('../config/config.json')[config]
-const sequelize = new Sequelize(db, {logging: false});
-
-module.exports = sequelize;
